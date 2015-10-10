e7701cc403f63a7e1776b2e762d52694bae960db
mikekhalil <mkhalil@purdue.edu>
2015-10-10 16:46:37 -0400

    First commit - Ruby on Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '408ff6f5a8e64692395c05b10bd90dc1b2ec7cf6b150673630e02454a45b99c1e0e8f09833f12e96b9fe3c70b5ea5d7d32897a8ffa6fbce7bae03491732b0150'
