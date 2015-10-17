8fb3e8170cd6d411435e35754ee387dcc1a1a799
Mikey Khalil <mkhalil@purdue.edu>
2015-10-17 17:47:34 -0400

    fixed update feature for machines

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,5 +1,24 @@
 class Machine < ActiveRecord::Base
   validates :name, presence: true, :uniqueness => true
+
+  def self.updateMachines
+	  	file = File.join(Rails.root, 'app',  'bigcomputeroutput.txt')
+		File.open(file).each_slice(4) do |four_lines|
+		if four_lines != nil
+			name = four_lines[0].chomp
+			cpu = four_lines[1].chomp.chomp.chomp.chomp
+			numUsers = four_lines[2].chomp
+			latencies = four_lines[3].chomp
+			m = Machine.where(name: name).first
+			if !m.nil?
+				m.cpuPercent = cpu.to_i
+				m.numUsers = numUsers.to_i
+				m.latencies = latencies.to_f
+				m.save
+			end
+		end
+
+  end
 end
 
 data = Machine.new
@@ -16,19 +35,5 @@ data.save
 #cpuPercent%
 #numUsers
 #latencies
-file = File.join(Rails.root, 'app',  'bigcomputeroutput.txt')
-File.open(file).each_slice(4) do |four_lines|
-	if four_lines != nil
-		name = four_lines[0].chomp
-		cpu = four_lines[1].chomp.chomp.chomp.chomp
-		numUsers = four_lines[2].chomp
-		latencies = four_lines[3].chomp
-		m = Machine.where(name: name).first
-		if !m.nil?
-			m.cpuPercent = cpu.to_i
-			m.numUsers = numUsers.to_i
-			m.latencies = latencies.to_f
-			m.save
-		end
-	end
+
 end
