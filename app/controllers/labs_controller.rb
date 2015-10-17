6e5e442f024e51b61a29c71eac710c8266024641
Mikey Khalil <mkhalil@purdue.edu>
2015-10-17 06:38:48 -0500

    fixed model

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -2,11 +2,13 @@ class LabsController < ApplicationController
 	def show
 		@lab = Labs.find(params[:id])
 		@computers = Computer.where(labRoom: @lab.roomNumber)
+		@computers = Computer.all
 	end
 
 	def index
 		@lawsonComputers = Labs.where(building: "Lawson")
 		@haasComputers = Labs.where(building: "Haas")
+		@computers = Computer.all
 	end
 
 end
